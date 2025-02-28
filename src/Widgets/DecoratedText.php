21a10bff14e87cbce68054dfe7c3e25ed4972db7
Miguel Graciá <miguel.gracia@homedoctor.es>
2025-02-28 12:50:07 +0100

    Adding wrapText function to DecoratedText class

--- src/Widgets/DecoratedText.php
+++ src/Widgets/DecoratedText.php
@@ -24,6 +24,10 @@ class DecoratedText extends AbstractWidget
         return $this;
     }
 
+    /**
+     * @param AbstractIcon $icon
+     * @return $this
+     */
     public function startIcon(AbstractIcon $icon): DecoratedText
     {
         $this->payload['startIcon'] = $icon;
@@ -32,18 +36,31 @@ class DecoratedText extends AbstractWidget
     }
 
     /**
+     * @param bool $value
+     * @return $this
+     */
+    public function wrapText(bool $value): DecoratedText
+    {
+        $this->payload['wrapText'] = $value;
+        return $this;
+    }
+
+    /**
      * Return a new Decorated Text widget instance.
      *
      * @param string $text
+     * @param bool $wrapText
      * @return self
      */
-    public static function create(string $text): DecoratedText
+    public static function create(string $text, bool $wrapText = true): DecoratedText
     {
         $widget = new static;
 
         if ($text) {
             $widget->text($text);
         }
+
+        $widget->wrapText($wrapText);
 
         return $widget;
     }
